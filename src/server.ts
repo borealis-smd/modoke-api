fedec8594ccd5197e2eb5fcca644ad7a19210c08
Joao Marcos <joao.barroso@alu.ufc.br>
2024-07-27 10:10:55 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Fastify, {FastifyReply, FastifyRequest} from 'fastify';
+import {config} from "dotenv";
+
+config();
+
+const server = Fastify({logger: true});
+const PORT = Number(process.env.PORT) || 3000;
+
+server.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    return {hello: 'world'};
+});
+
+server.listen({port: PORT}, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
