1bd39490edbdb377d3ca4764b8fb17b899ec838c
Joao Marcos <joao.barroso@alu.ufc.br>
2024-08-30 11:52:05 -0300

    add: upload route protection

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { uploadImage } from "../controllers/uploadController";
+import { verifyTokenMiddleware } from "../middleware/authMiddleware";
 
 export default function UploadRoutes(
   fastify: FastifyInstance,
   options: any,
   done: Function,
 ) {
-  fastify.post("/", uploadImage);
+  fastify.post("/", { preHandler: verifyTokenMiddleware() }, uploadImage);
 
   done();
 }
